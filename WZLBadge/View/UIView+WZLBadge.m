c6c8da8ac06b687f95fdcb8b936bd236d5ad7a74
Daniel Lin <linzhdk@gmail.com>
2018-02-06 17:43:57 +0800

    Adjustment for iOS 11, making badge display on the top of its siblings when its super view appears every time.

--- WZLBadge/View/UIView+WZLBadge.m
+++ WZLBadge/View/UIView+WZLBadge.m
@@ -19,6 +19,33 @@ static const CGFloat kWZLBadgeDefaultRedDotRadius = 4.f;
 
 @implementation UIView (WZLBadge)
 
+#pragma mark - Swizzling
++ (void)load {
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        Class class = [super class];
+        SEL originalSelector = @selector(willMoveToSuperview:);
+        SEL swizzledSelector = @selector(wzl_willMoveToSuperview:);
+        Method originalMethod = class_getInstanceMethod(class, originalSelector);
+        Method swizzledMethod = class_getInstanceMethod(class, swizzledSelector);
+        method_exchangeImplementations(originalMethod, swizzledMethod);
+    });
+}
+
+
+/**
+ By swizzling the original method - (void)willMoveToSuperview:(UIView *)newSuperview;
+ Move the badge view on top of its siblings when its super view appears every time for adjustment on iOS 11.
+
+ */
+- (void)wzl_willMoveToSuperview:(UIView *)newSuperview {
+    [self wzl_willMoveToSuperview:newSuperview];
+    
+    if (newSuperview && self.badge) {
+        [self bringSubviewToFront:self.badge];
+    }
+}
+
 #pragma mark -- public methods
 /**
  *  show badge with red dot style and WBadgeAnimTypeNone by default.
